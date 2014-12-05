715b4390cd8287dfeb8b49d6d0bc7b6a231dae0d
byrenx <jofel.bayron@cloudsherpas.com>
2014-12-05 22:17:53 +0800

    latest

--- app/components/mails.py
+++ app/components/mails.py
@@ -10,4 +10,31 @@ class Mails(object):
         self.controller = controller
         mail = Mail()
 
-        
+    def index(self):
+        data = memcache.get('mail_results')
+        mail = []
+        if data is None:
+            source = 'From Datastore'
+            data = self.mail.list()
+            memcache.add('mail_results', data, 60)
+        else:
+            source = 'From Memcache'
+
+        if self.controller.request.method=='POST':
+            subject = self.controller.request.params['subject']
+            for item in data:
+                if item.subject == subject and item.recipient == email:
+                    mail.append(item)
+        else:
+            for item in data:
+                if item.recipient == email:
+                    mail.append(item)
+            
+
+        self.controller.session['user']    = email
+        self.controller.context['source']  = source
+        self.controller.context['email']   = email
+        self.controller.context['inboxes'] = mail
+
+
+    
