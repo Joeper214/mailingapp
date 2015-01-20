7eeb76c62d66efe66e3321a8fd706bc16d3ce57d
byrenx <jofel.bayron@cloudsherpas.com>
2015-01-20 22:02:42 +0800

    backup for mailing app for google app engine during intership

---
+++ app/static/js/list.js
@@ -0,0 +1,35 @@
+function listController(){
+    $scope.themes = {"3024-day",
+		     "3024-night",
+		     "ambiance-mobile",
+		     "ambiance", 
+		     "base16-dark",
+		     "base16-light",
+		     "blackboard",
+		     "cobalt",
+		     "eclipse",
+		     "elegant", 
+		     "erlang-dark",
+		     "lesser-dark",
+		     "mbo",
+		     "mdn-like",
+		     "midnight",
+		     "monokai",
+		     "neat",
+		     "neo",
+		     "night",
+		     "paraiso-dark",
+		     "paraiso-light",
+		     "pastel-on-dark",
+		     "rubyblue",
+		     "solarized",
+		     "the-matrix",
+		     "tomorrow-night-eighties",
+		     "twilight",
+		     "vibrant-ink",
+		     "xq-dark",
+		     "xq-light"}
+
+    $scope.keymap = {'sublime', 'emacs', 'vim'}
+    $scope.mode = {'python','javascript'}
+}
